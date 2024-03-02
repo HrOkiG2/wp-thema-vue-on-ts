ac82e8d767d206df0e20ea201091ef7247732492
ryo <mari-mac.com>
2024-03-03 07:48:34 +0900

    👏 DaisyUI introduce

--- wordpress_data/wp-content/themes/wp-thema-vue-on-ts/src/App.vue
+++ wordpress_data/wp-content/themes/wp-thema-vue-on-ts/src/App.vue
@@ -2,6 +2,21 @@
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="Shoes"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
     <p class="h-screen bg-red-700 text-green-900">aaaaaaa</p>
   </div>
 </template>
